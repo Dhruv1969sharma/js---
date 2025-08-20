3c46e75c341e1f5e91c572101f1307a6cf807b2c
Dhruv1969sharma <dhruv1969sharma@gmail.com>
2025-08-20 16:11:37 +0530

    01basicas

--- 01basicas/01variable.js
+++ 01basicas/01variable.js
@@ -2,7 +2,9 @@ const accountid = 144454656
 let axxountemail = "dhruv696sharma696@gmail.com"
 var accountpassword = "54321"
 accountcity = "jaipur"
-  
+
+let accountState=
+
 accountEmail = "dhruv40900"
 accountpassword="dhruv55"
 accountcity= "himachal"
